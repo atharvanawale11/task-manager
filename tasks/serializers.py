3149d07fc73798f4538a8b0aaf67ec0f9806252d
Atharva Nawale <atharvarnawale@gmail.com>
2026-08-25 22:48:09 +0530

    feat: Add Serialize tor converts the Django object into this JSON-compatible format.

---
+++ tasks/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            'id', 'title', 'description', 'priority',
+            'status', 'due_date', 'user', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['user', 'created_at', 'updated_at']
